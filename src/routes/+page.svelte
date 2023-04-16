4cb9ecfe6d2894b92efd7c735c50868698a06cc1
Jess Martin <jessmartin@gmail.com>
2023-04-16 19:08:41 -0400

    Adding a copy to clipboard button

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -135,7 +135,36 @@
       on:click={() => (markupToggle = 'markdown')}>Markdown</button
     ></span
   >
+  <span class="text-gray-400 float-right">
+    <button
+      class="p-2 font-mono rounded-md bg-gray-200 dark:bg-slate-700 hover:bg-slate-300 hover:dark:bg-slate-600"
+      on:click={() => {
+        const url = new URL(window.location.href)
+        url.searchParams.set('annotations', JSON.stringify(doc.annotations))
+        url.searchParams.set('text', doc.text)
+        navigator.clipboard.writeText(url.toString())
+      }}
+    >
+      <svg
+        aria-hidden="true"
+        height="16"
+        viewBox="0 0 16 16"
+        version="1.1"
+        width="16"
+        data-view-component="true"
+        class="inline-block stroke-gray-400"
+      >
+        <path
+          d="M0 6.75C0 5.784.784 5 1.75 5h1.5a.75.75 0 0 1 0 1.5h-1.5a.25.25 0 0 0-.25.25v7.5c0 .138.112.25.25.25h7.5a.25.25 0 0 0 .25-.25v-1.5a.75.75 0 0 1 1.5 0v1.5A1.75 1.75 0 0 1 9.25 16h-7.5A1.75 1.75 0 0 1 0 14.25Z"
+        /><path
+          d="M5 1.75C5 .784 5.784 0 6.75 0h7.5C15.216 0 16 .784 16 1.75v7.5A1.75 1.75 0 0 1 14.25 11h-7.5A1.75 1.75 0 0 1 5 9.25Zm1.75-.25a.25.25 0 0 0-.25.25v7.5c0 .138.112.25.25.25h7.5a.25.25 0 0 0 .25-.25v-7.5a.25.25 0 0 0-.25-.25Z"
+        />
+      </svg>
+      Copy URL</button
+    ></span
+  >
 </p>
+
 <div
   class="mb-1 pb-1 overflow-x-auto whitespace-nowrap h-64 {markupToggle === 'markdown'
     ? ''
